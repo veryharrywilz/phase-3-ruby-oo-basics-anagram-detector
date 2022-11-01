0fd83493185b1353b0a2779dd6af5b61a2f94c0c
veryharrywilz <110489443+veryharrywilz@users.noreply.github.com>
2022-11-01 16:00:49 -0400

    commit tree d1bb4838d20a18075536d55c367132cba89cb229

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    def initialize(word)
+        @word=word
+    end
+
+
+    def match(compareWords)
+        wordsThatMatch = []
+        inputWordArray = @word.split("").sort
+        compareWords.map do |word|
+            wordArray = word.split("").sort
+            if wordArray == inputWordArray
+                wordsThatMatch.push(word)
+            elsif wordArray != inputWordArray
+                "detects no matches"
+            end
+        end
+        wordsThatMatch
+    end
+end
+
+binding.pry
